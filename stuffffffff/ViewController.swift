72f7ff313fc51ea78535804ef045706e12713631
kingsplatter <andrew.strange@nwktc.edu>
2015-01-07 08:49:03 -0700

    WHAT UP

--- stuffffffff/ViewController.swift
+++ stuffffffff/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//WHAT UP ADAM!!??!?!?!?
+    //CHECK IT OUT...I CHANGED UR PROJECT!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
